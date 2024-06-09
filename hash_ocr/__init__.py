bdfd8c3191e79d1c2bd1329a41ba58f2e5b275da
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-06-09 11:36:36 +0545

    feat: add md5 method

--- hash_ocr/__init__.py
+++ hash_ocr/__init__.py
@@ -1,5 +1,134 @@
-from hash_ocr.ocr import compute_distances
-from hash_ocr.ocr import get_characters
-from hash_ocr.ocr import get_word
+import hashlib
+import json
+from functools import lru_cache
+from pathlib import Path
+from typing import Literal
+from typing import Optional
 
-__all__ = ["compute_distances", "get_characters", "get_word"]
+import cv2
+import numpy as np
+from cv2.typing import MatLike
+
+Method = Literal["average", "block_mean", "md5"]
+
+BASE_DIR = Path(__file__).parent
+DEFAULT_MODEL = str(BASE_DIR / "model" / "digits.png")
+
+
+avg_hash = cv2.img_hash.AverageHash.create()
+block_mean_hash = cv2.img_hash.BlockMeanHash.create()
+
+
+# BlockMeanHash is 10x slower than AverageHash
+def compute_hash(img: MatLike, method: Method):
+    if method == "average":
+        return avg_hash.compute(img).tobytes()
+    if method == "block_mean":
+        return block_mean_hash.compute(img).tobytes()
+    if method == "md5":
+        return hashlib.md5(img.tobytes()).digest()
+    raise NotImplementedError(f"Hash method {method} not implemented.")
+
+
+def compare_hash(hsh1: bytes, hsh2: bytes, method: Method):
+    if method == "average":
+        return avg_hash.compare(np.frombuffer(hsh1), np.frombuffer(hsh2))
+    if method == "block_mean":
+        return block_mean_hash.compare(
+            np.frombuffer(hsh1), np.frombuffer(hsh2)
+        )
+    if method == "md5":
+        return 0.0 if hsh1 == hsh2 else float("inf")
+    raise NotImplementedError(f"Hash method {method} not implemented.")
+
+
+def threshold_image(img: MatLike) -> MatLike:
+    white = np.array([255, 255, 255])
+    img = cv2.inRange(img, white, white)
+    return img
+
+
+@lru_cache
+def get_model(
+    file_path: str = DEFAULT_MODEL,
+    method: Method = "block_mean",
+):
+    label_file_path = file_path.replace(".png", ".txt")
+    with open(label_file_path) as fp:
+        labels: list[Optional[str]] = json.load(fp)
+
+    img = cv2.imread(file_path)
+    img = threshold_image(img)
+    cnts, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = list(cnts)
+    cnts.sort(key=lambda x: cv2.boundingRect(x)[0])
+
+    output: list[tuple[str, bytes]] = []
+    for i, cnt in enumerate(cnts):
+        char = labels[i]
+        if char is None:
+            continue
+        x, y, w, h = cv2.boundingRect(cnt)
+        char_img: MatLike = img[y : y + h, x : x + w]
+        hsh = compute_hash(char_img, method=method)
+        output.append((char, hsh))
+    return output
+
+
+def compute_distances(
+    threshed_img: MatLike,
+    model_path: str = DEFAULT_MODEL,
+    method: Method = "block_mean",
+) -> list[list[tuple[str, float]]]:
+    model = get_model(file_path=model_path, method=method)
+    cnts, _ = cv2.findContours(
+        threshed_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    cnts = list(cnts)
+    cnts.sort(key=lambda x: cv2.boundingRect(x)[0])
+    output: list[list[tuple[str, float]]] = []
+    for cnt in cnts:
+        x, y, w, h = cv2.boundingRect(cnt)
+        letter_img: MatLike = threshed_img[y : y + h, x : x + w]
+        hsh = compute_hash(letter_img, method=method)
+        data = [
+            (char, compare_hash(hsh, hsh1, method)) for char, hsh1 in model
+        ]
+        data.sort(key=lambda d: d[1])
+        output.append(data)
+    return output
+
+
+def get_characters(
+    threshed_img: MatLike,
+    model_path: str = DEFAULT_MODEL,
+    max_dist: int = 80,
+    method: Method = "block_mean",
+) -> list[tuple[str, float]]:
+    output: list[tuple[str, float]] = []
+    distances = compute_distances(
+        threshed_img, model_path=model_path, method=method
+    )
+    for data in distances:
+        data = [d for d in data if d[1] <= max_dist]
+        if data == []:
+            continue
+        best = data[0]
+        output.append(best)
+    return output
+
+
+def get_word(
+    threshed_img: MatLike,
+    model_path: str = DEFAULT_MODEL,
+    max_dist: int = 80,
+    method: Method = "block_mean",
+) -> str:
+    """
+    This function assumes that the image contains only one word
+    """
+    chars = get_characters(
+        threshed_img, model_path=model_path, max_dist=max_dist, method=method
+    )
+    text = "".join(char for char, _ in chars)
+    return text
